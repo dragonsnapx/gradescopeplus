87a9b87a82cc66ce7590d20a864eac4fa24d9673
Jake Lee <51874481+dragonsnapx@users.noreply.github.com>
2021-09-18 14:16:47 -0400

    new: settings working now

--- src/pages/shared/settings.js
+++ src/pages/shared/settings.js
@@ -2,30 +2,21 @@ import React from 'react';
 import './settings.css';
 
 class Settings extends React.Component{
+  constructor(props) {
+      super(props);
 
-    constructor(props) {
-        super(props);
+      this.state = {
+          blockerList: []
+      }
+  }
 
-        this.state = {
-            blockerList: []
-        }
-
-        this.changeCheckbox = this.changeCheckbox.bind(this);
-        this.save = this.save.bind(this);
-        this.reset = this.reset.bind(this);
-    }
-
-    render(){
-        return(
-            <div id='app'>
-                <h1>FMBlocker 블라인드 리스트 설정</h1>
-                <br />
-                <button onClick={this.save}>저장</button>
-                <button onClick={this.reset}>모두 삭제</button>
-            </div>
-        );
-    }
-
+  render(){
+    return(
+      <div id='app'>
+        <h1>Gradescope+</h1>
+      </div>
+    );
+  }
 }
 
 export default Settings;
